450bc678f66f1ea0724c0ea3aec8cded15d32eba
dazrwf <anonymoous.xo@gmail.com>
2024-02-23 03:51:03 -0800

    1st

---
+++ JavaScript/main.js
@@ -0,0 +1,34 @@
+let num = prompt("Enter a number:");
+function comput(num) {
+
+  let result = 0;
+for (let p = 0; p <= num; p++) {
+  result += p;
+}
+ document.write(`<h2 class="sum">The sum of ${num} numbers is ${result}</h2>`)
+  let sum = 0;
+  for (let j = 1; j < num; j++) {
+    if (num % j === 0) {
+      sum += j;
+    }
+  }
+  if (sum === num) {
+    document.write(`<h2 class="perfect">${num} is a perfect number</h2>`);
+  } else {
+    document.write(`<h2 class="perfect">${num} is not perfect number</h2>`);
+  }
+
+  let y = 0
+  for (let i = 1; i <= num; i++) {
+    if (num % i === 0) {
+      y++
+    }
+  }
+  if (y === 2) {
+    document.write(`<h2 class="prime">${num} is Prime</h2>`)
+  
+  } else {
+    document.write(`<h2 class="prime">${num} is not Prime</h2>`)
+  }
+}
+comput(num)
